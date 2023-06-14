c5b2f92fad039588d1b55c3eb8cb6b4260ee4b3e
siobhain <>
2023-06-14 22:05:20 +0000

    add sqlalchemy expression level statements

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -13,7 +13,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT "Name" FROM "Artist"')
 
 # Query 3 - select only "Queen" from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
 # cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
@@ -22,7 +22,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["*zepp*"])
+#cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["*zepp*"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
